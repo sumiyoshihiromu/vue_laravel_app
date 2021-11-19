0ddf8487054eb90ac509443ab8f33d9705401a16
sumiyoshihiromu <sumihiro127@icloud.com>
2021-11-20 01:01:25 +0900

    ヘッダーコンポーネントまでできた

---
+++ resources/js/components/HeaderComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container-fluid bg-dark mb-3">
+    <div class="container">
+      <nav class="navbar navbar-dark">
+        <span class="navbar-brand mb-0 h1">TO DO APPだよ</span>
+        <div>
+          <button class="btn btn-success">一覧</button>
+          <button class="btn btn-success">タスク追加</button>
+        </div>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
