cf4caf11b7ba0e2e8159f376f56a17629b233de8
sumiyoshihiromu <sumihiro127@icloud.com>
2021-11-28 23:47:37 +0900

    タスク詳細apiをいい感じに

--- resources/js/components/TaskShowComponent.vue
+++ resources/js/components/TaskShowComponent.vue
@@ -5,19 +5,19 @@
         <form>
           <div class="form-group row border-bottom">
             <label for="id" class="col-sm-3 col-form-label">ID</label>
-            <input type="text" class="col-sm-9 form-control-plaintext" readonly id="id" :value="taskId">
+            <input type="text" class="col-sm-9 form-control-plaintext" readonly id="id" v-model="task.id">
           </div>
           <div class="form-group row border-bottom">
             <label for="title" class="col-sm-3 col-form-label">タイトル</label>
-            <input type="text" class="col-sm-9 form-control-plaintext" readonly id="title" :value="taskId + 'のタイトル'">
+            <input type="text" class="col-sm-9 form-control-plaintext" readonly id="title" v-model="task.title">
           </div>
           <div class="form-group row border-bottom">
             <label for="content" class="col-sm-3 col-form-label">やること</label>
-            <input type="text" class="col-sm-9 form-control-plaintext" readonly id="content" :value="taskId + 'のやること'">
+            <input type="text" class="col-sm-9 form-control-plaintext" readonly id="content" v-model="task.content">
           </div>
           <div class="form-group row border-bottom">
             <label for="person-in-charge" class="col-sm-3 col-form-label">担当者</label>
-            <input type="text" class="col-sm-9 form-control-plaintext" readonly id="person-in-charge" value="Hiromu">
+            <input type="text" class="col-sm-9 form-control-plaintext" readonly id="person-in-charge" v-model="task.person_in_charge">
           </div>
         </form>
       </div>
@@ -29,6 +29,21 @@
   export default {
     props: {
       taskId: String
+    },
+    data: function () {
+      return {
+        task: {}
+      }
+    },
+    methods: {
+      getTask() {
+        axios.get('/api/tasks/' + this.taskId).then((res) => {
+          this.task = res.data;
+        });
+      }
+    },
+    mounted() {
+      this.getTask();
     }
   }
 </script>
